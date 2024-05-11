002588487cb5e542dd01d66949b8aa1f78dc0d9d
bensoil <157688179+bensoil@users.noreply.github.com>
2024-05-11 15:53:21 +0200

    Add project files.

---
+++ RETURN/Program.cs
@@ -0,0 +1,16 @@
+﻿int EttTall = 1;
+
+int EttAnnetTall = 2;
+
+int SumOfHisParts()
+{
+    return EttTall + EttAnnetTall;
+   
+}
+
+void TurnBackTime()
+{
+Console.WriteLine("Denne metoden returnerer ingenting.");
+}
+
+TurnBackTime();
